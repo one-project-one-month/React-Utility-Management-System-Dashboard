ec08678c870a9fe0afd7decd5f588080adaef7bd
Wunna <wunna3117@gmail.com>
2025-09-27 23:17:28 +0700

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router"
-import { useRoutes } from "./hooks/useRoutes"
+import { useRoutes } from "@/hooks/useRoutes"
 import { Suspense } from "react"
 
 function App() {
