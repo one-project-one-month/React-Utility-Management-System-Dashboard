e7c550ee1ba690308dcdf93f7e87609daf88c238
sisaung <sisaungvipse780@gmail.com>
2025-10-04 10:22:36 +0630

    fix: add logout

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,24 +1,52 @@
 import {
-  Input, Dropdown,
+  Input,
+  Dropdown,
   DropdownTrigger,
   DropdownMenu,
   DropdownSection,
   DropdownItem,
-  Avatar
-} from '@heroui/react'
-import { Search, User, Settings, LogOutIcon } from 'lucide-react'
-import ThemeToggle from '@/components/theme-toggle'
+  Avatar,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+} from "@heroui/react";
+import {
+  Search,
+  User,
+  Settings,
+  LogOutIcon,
+  LucideTriangleAlert,
+} from "lucide-react";
+import ThemeToggle from "@/components/theme-toggle";
+import useLogout from "@/hooks/useLogout";
 
 export default function Header() {
+  const { mutate } = useLogout();
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const handleLogoutBtn = () => {
+    onOpen();
+  };
+
+  const handleConfirmLogout = () => {
+    mutate();
+    onClose();
+  };
+
   return (
-    <header className="flex h-14 items-center gap-4 border-b border-border bg-card px-6">
+    <header className="flex items-center gap-4 px-6 border-b h-14 border-border bg-card">
       <div>
         <h1>NestFlow</h1>
       </div>
 
-      <div className="flex flex-1 items-center gap-4">
-        <div className="relative max-w-md flex-1">
-          <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 text-muted-foreground" />
+      <div className="flex items-center flex-1 gap-4">
+        <div className="relative flex-1 max-w-md">
+          <Search className="absolute w-4 h-4 -translate-y-1/2 left-3 top-1/2 text-muted-foreground" />
           <Input placeholder="Search..." className="pl-9" />
         </div>
       </div>
@@ -29,17 +57,21 @@ export default function Header() {
         <Dropdown>
           <DropdownTrigger>
             <Avatar
-              className="h-8 w-8 cursor-pointer"
+              className="w-8 h-8 cursor-pointer"
               src="https://i.pravatar.cc/150?u=a042581f4e29026024d"
             />
           </DropdownTrigger>
           <DropdownMenu aria-label="User Menu" variant="faded">
             <DropdownSection showDivider>
-              <DropdownItem key='username' isReadOnly>
-                <span className="text-sm font-medium leading-none">Admin User</span>
+              <DropdownItem key="username" isReadOnly>
+                <span className="text-sm font-medium leading-none">
+                  Admin User
+                </span>
               </DropdownItem>
-              <DropdownItem key='email' isReadOnly>
-                <span className="text-xs text-muted-foreground">admin@example.com</span>
+              <DropdownItem key="email" isReadOnly>
+                <span className="text-xs text-muted-foreground">
+                  admin@example.com
+                </span>
               </DropdownItem>
             </DropdownSection>
 
@@ -58,13 +90,48 @@ export default function Header() {
                 className="text-danger"
                 color="danger"
                 startContent={<LogOutIcon />}
+                onPress={handleLogoutBtn}
               >
                 Logout
               </DropdownItem>
             </DropdownSection>
           </DropdownMenu>
         </Dropdown>
+        <Modal isOpen={isOpen} size="sm" onClose={onClose}>
+          <ModalContent>
+            {(onClose) => (
+              <>
+                <ModalHeader className="flex flex-col items-center justify-center gap-2">
+                  <LucideTriangleAlert className="text-danger" size={28} />
+                  <span className="text-2xl">Logout?</span>
+                </ModalHeader>
+                <ModalBody className="text-gray-600">
+                  Are you sure you want to log out? You'll need to login in
+                  again to access your account.
+                </ModalBody>
+                <ModalFooter>
+                  <div className="flex items-center w-full gap-x-1 ">
+                    <Button
+                      className="w-full"
+                      variant="light"
+                      onPress={onClose}
+                    >
+                      Cancel
+                    </Button>
+                    <Button
+                      color="danger"
+                      className="w-full"
+                      onPress={handleConfirmLogout}
+                    >
+                      Confirm
+                    </Button>
+                  </div>
+                </ModalFooter>
+              </>
+            )}
+          </ModalContent>
+        </Modal>
       </div>
     </header>
-  )
+  );
 }
--- src/services/authApi.ts
+++ src/services/authApi.ts
@@ -5,3 +5,8 @@ export const login = async (formData: TLoginSchema): Promise<LoginResponse> => {
   const res = await axiosInstance.post<LoginResponse>("auth/login", formData);
   return res.data;
 };
+
+export const logout = async () => {
+  const res = await axiosInstance.post("auth/logout");
+  return res.data;
+}
