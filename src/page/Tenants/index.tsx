fffd6a4444aa62d4d4c500b043744ebc15be7906
shinnThantKyaw <shinthantkyaw1818@gmail.com>
2025-10-10 20:52:54 +0630

    feat:tenant-management-added-navigation-bread-crumbs

--- src/page/Tenants/index.tsx
+++ src/page/Tenants/index.tsx
@@ -1,11 +1,17 @@
 import TenantsListHeader from "@/components/Tenants/TenantsPage/tenants-list-header.tsx";
 import TenantsTableContainer from "@/components/Tenants/TenantsPage/tenants-table-container.tsx";
+import NavigationBreadCrumbs from "@/components/breadcrumb.tsx";
 
 export default function TenantsPage() {
   return (
     <div className="h-[84vh] px-2 overflow-y-auto custom-scrollbar">
       <div className="min-h-[90vh]  rounded-xl    dark:border-white/[0.05] dark:bg-white/[0.03]">
-        <h1 className="font-roboto font-bold text-2xl">Tenants</h1>
+        <NavigationBreadCrumbs
+          items={[
+            { label: "Home", href: "/" },
+            { label: "Tenants", href: "tenants" },
+          ]}
+        />
         <TenantsListHeader />
 
         <div className="h-[68vh] overflow-y-auto rounded-xl  dark:bg-white/[0.03] custom-scrollbar">
